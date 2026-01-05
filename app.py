e74b760ce08d41216784cf8bc99ece4e5137625c
Ninjainpaijama1234 <shauryashikhar1398@gmail.com>
2026-01-05 11:11:41 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-```python
 import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
-import plotly.graph_objects as go
 from sklearn.ensemble import RandomForestRegressor
 import warnings
 
